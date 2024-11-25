4a7fc8c209b3e06711af60f52ec30f079251fb5b
Lord Paramount <LordParamount@icloud.com>
2024-11-24 19:57:29 -0700

    Updated to have file for doing visuals, added visual and graph display support into sources

---
+++ utils/CrossJoin_Visuals.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+def GraphSources(XAxisName: str, YAxisName: str, Data: list, Data2: list):
+    fig, ax = plt.subplots()
+    Count = 0
+    for z in Data:
+        ax.plot(z, label=Data2[Count])
+        Count += 1
+    plt.xlabel(XAxisName)
+    plt.ylabel(YAxisName)
+    plt.legend(loc="upper left")
+    plt.savefig("CacheFile.png")
+    return Image.open("CacheFile.png").tobytes()
