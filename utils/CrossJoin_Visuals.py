54885f4e0c3eda4dae23192fbe2b6bd0479d683d
SouthAlbertaAI <111461851+SouthAlbertaAI@users.noreply.github.com>
2024-11-27 20:12:01 -0700

    Added additional visual, made some updates to the other one (#8)

--- utils/CrossJoin_Visuals.py
+++ utils/CrossJoin_Visuals.py
@@ -46,6 +46,20 @@ def GraphSources(XAxisName: str, YAxisName: str, Data: list, Data2: list):
         Count += 1
     plt.xlabel(XAxisName)
     plt.ylabel(YAxisName)
-    plt.legend(loc="upper left")
+    plt.legend(loc="upper right")
+    plt.savefig("CacheFile.png")
+    return Image.open("CacheFile.png").tobytes()
+
+
+def GraphReference(CurrentPowerUsage: int, MaxGeneration: int):
+    fix, ax = plt.subplots()
+    DangerZone = (0.8 * (MaxGeneration))
+    ax.plot([CurrentPowerUsage, CurrentPowerUsage], label="Current Power Usage")
+    ax.plot([MaxGeneration, MaxGeneration], label="Max Power Capacity")
+    ax.plot([DangerZone, DangerZone], label="Danger Zone")
+    plt.ylim(bottom=5000)
+    plt.legend(loc="upper right")
+    plt.xlabel("Power Thresholds")
+    plt.ylabel("Power Usage Megawatts")
     plt.savefig("CacheFile.png")
     return Image.open("CacheFile.png").tobytes()
